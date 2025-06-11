a6d11776b54febbc0b87507cf8857b53c448f49d
TheDARKOMETER <thebeangodzilla22@gmail.com>
2025-06-11 16:03:27 +0100

    Websocket can establish connection with server. TODO: Send message to client on connection

--- src/java/com/jsocket/config/WebSocketConfiguration.java
+++ src/java/com/jsocket/config/WebSocketConfiguration.java
@@ -9,20 +9,24 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.stereotype.Controller;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
 
-	@Override
-	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config.enableSimpleBroker("/topic");
-		config.setApplicationDestinationPrefixes("/app");
-	}
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+        System.out.println("Enabling broker and setting prefix");
+    }
 
-	@Override
-	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/jsocketapi");
-	}
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/javafxchat").setAllowedOriginPatterns("*");
+        System.out.println("Adding endpoint");
+
+    }
 
 }
